3747838c50def4b0d4ccc2fccd2a214655035c79
lucas123mota <soulucasmota@gmail.com>
2025-07-30 16:30:06 -0300

    ajuste introdução

--- src/app/componentes/contato/contato.component.ts
+++ src/app/componentes/contato/contato.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router'; 
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [FormsModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './contato.component.html',
   styleUrls: ['./contato.component.css']
 })
@@ -13,13 +15,20 @@ export class ContactComponent {
   email = '';
   message = '';
 
+  constructor(private router: Router) {} 
+
   sendMessage() {
     console.log('Name:', this.name);
     console.log('Email:', this.email);
     console.log('Message:', this.message);
-    alert('Mensagem enviada com sucesso!');
+    
+
+    
     this.name = '';
     this.email = '';
     this.message = '';
+
+    
+    this.router.navigate(['/login']);
   }
 }
